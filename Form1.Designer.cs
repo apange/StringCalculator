2589601ff1467cd135e31df9532b079f78f17bc4
apange <pange.akshaya@gmail.com>
2015-01-16 14:45:09 +0530

    Upload For the first time
    Upload For the first time

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace StringCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStringInput = new System.Windows.Forms.RichTextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter String Number:";
+            // 
+            // txtStringInput
+            // 
+            this.txtStringInput.Location = new System.Drawing.Point(132, 41);
+            this.txtStringInput.Name = "txtStringInput";
+            this.txtStringInput.Size = new System.Drawing.Size(251, 96);
+            this.txtStringInput.TabIndex = 1;
+            this.txtStringInput.Text = "";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(132, 181);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 265);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtStringInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txtStringInput;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ Logic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Logic : ICalculator
+    {
+        public  int add(string numbers)
+        {
+            try
+            {
+                int sum = 0;
+                if (numbers == "" || numbers.Length == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    string no_modified = Regex.Replace(numbers, @"[^0-9]+", ",");
+                    string[] no_array = no_modified.Split(',');
+                    foreach (string no in no_array)
+                    {
+                        sum = sum + Convert.ToInt32(no);
+                    }
+                    return sum;
+                }
+            }
+
+            catch (FormatException)
+            {
+                return -1;
+            }
+
+            catch (Exception)
+            {
+                return -2;
+            }
+
+        }
+    }
+}
